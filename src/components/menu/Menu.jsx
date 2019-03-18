1d238ab2baa7871b0a482fc64842228b94f76b4e
Dmytro <vladimyrovych@gmail.com>
2019-03-18 21:44:30 +0200

    add logic: remove city from localStorage

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -14,7 +14,6 @@ export class Menu extends React.Component {
     }
 
     onClick = (cityName, cityKey) => {//добавление города в избранные
-        this.setState({currentValue: cityName});
         if (localStorage.getItem('favoriteCities') === null) {
             let favoriteCities = {
                 [cityKey]: cityName,
@@ -25,13 +24,17 @@ export class Menu extends React.Component {
             currentStorage[cityKey] = cityName;
             localStorage.setItem('favoriteCities', JSON.stringify(currentStorage));
         }
-        
+        this.setState({currentValue: cityName});
+    }
+
+    removeFavoriteCity = () => {
+        this.setState({currentValue: ''});
     }
 
     render() {
-        let hiddenClassname = 'menu__block';
+        let hiddenClassName = 'menu__block';
         if (this.state.isHidden === true) {
-            hiddenClassname += ' menu__block_hidden';
+            hiddenClassName += ' menu__block_hidden';
         }
 
         let favoriteCities = [];
@@ -40,18 +43,22 @@ export class Menu extends React.Component {
             for (const key in favoriteCitiesStorage) {
                 if (favoriteCitiesStorage.hasOwnProperty(key)) {
                     const element = favoriteCitiesStorage[key];
-                    favoriteCities.push(<FavoriteCity cityName={element}/>);
+                    favoriteCities.push(
+                        <FavoriteCity 
+                            cityName={element} 
+                            cityKey={key}
+                            removeFavoriteCity={this.removeFavoriteCity}
+                        />
+                    );
                 }
             }
         }
         
         return (
             <div className='weather__menu menu'>
-                <div className={hiddenClassname}>
+                <div className={hiddenClassName}>
                     <SearchForm placeholder='Find city...' onClick={this.onClick}/>
-                    <div className='menu__favorite-city-container'>
-                        {favoriteCities}
-                    </div>
+                    {favoriteCities}
                 </div>
                 <button className='menu__open-btn' onClick={this.showMenu}>
                     <div className='menu__btn-line'></div>
--- src/components/menu/favoriteCity/FavoriteCity.jsx
+++ src/components/menu/favoriteCity/FavoriteCity.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import './FavoriteCity.scss';
 
 export class FavoriteCity extends React.Component {
+    removeFavoriteCity = () => {
+        let currentStorage = JSON.parse(localStorage.getItem('favoriteCities'));
+        delete currentStorage[this.props.cityKey]
+        localStorage.setItem('favoriteCities', JSON.stringify(currentStorage));
+        this.props.removeFavoriteCity();
+    }
+
     render() {
         return (
             <div className='menu__favorite-city favorite-city'>
                 <button className='favorite-city__name-btn'>{this.props.cityName}</button>
-                <button className='favorite-city__close-btn'>X</button>
+                <button className='favorite-city__close-btn' onClick={this.removeFavoriteCity}>X</button>
             </div>
         )
     }
